bd26f6cda8e83a464efc6edcdbdaeb2307112a8d
Elodie Jolo <elodie.jolo@gmail.com>
2024-05-02 22:39:18 +0200

    install project + access

--- script.sh
+++ script.sh
@@ -11,3 +11,9 @@ sudo apt-get install -y docker.io
 
 # Ajouter l'utilisateur actuel au groupe Docker pour éviter d'utiliser sudo pour les commandes Docker
 sudo usermod -aG docker $USER
+
+sudo apt-get install -y nodejs npm
+git clone https://github.com/johnpapa/node-hello.git,  # Remplacez l'URL du dépôt par celui que vous souhaitez cloner
+cd node-hello,
+npm install,  # Assurez-vous d'adapter cette commande selon votre projet (ex: yarn install, composer install, etc.)
+npm start
